9d219f4be54d5fa5e105498dd2e1a5d8bd9b8bc8
Austin <aw2793@nyu.edu>
2020-04-29 16:22:24 -0700

    adding all components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,351 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class Calculator extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.calculatorOnClickHandler = this.calculatorOnClickHandler.bind(this);
+    this.state = {expression: '0', result: 0, startNum: true};
+  }
+
+  calculatorOnClickHandler (value) {
+
+    switch(value){
+
+      // reset everything
+      case "AC":
+          this.setState({
+            result: 0,
+            expression: '0',
+            startNum: true,  
+            
+          })
+          break;
+
+      
+      case "+":
+          this.setState({
+            expression: this.state.expression + "+",        
+            
+          })
+          break;
+      
+      case "-":
+          this.setState({
+            expression: this.state.expression + "-",  
+           
+          })
+          break;
+      
+
+      case "*":
+
+          this.setState({
+            expression: this.state.expression + "*",         
+           
+          })
+          break;
+      
+      case "%":
+          this.setState({
+            expression: this.state.expression + "%",
+           
+          })
+          break;
+      
+      
+      case "=":
+
+          // check for errors in the expression, if found display error 
+          var errorExists = errorCheck(this.state.expression)
+          if (errorExists === true){
+            this.setState({
+              expression: "ERROR",
+              startNum: true,
+              result: 0,
+                
+                })
+            
+            break;
+          }
+
+          // parse the expression into an array 
+          var expression = parser(this.state.expression);
+
+          // calculate the result, or return an error if 0 division error is found 
+          var result = calculateResult(expression);
+
+          this.setState({
+            expression: result,
+              })
+
+          break;
+        
+
+      default:
+
+        // if first num, get rid of 0 at the beginning
+        if (this.state.startNum === true && this.state.expression === "0-"){
+          this.setState({
+            expression: "-" + value,
+            startNum: false,
+            })
+        }
+
+        else if (this.state.startNum === true){
+          this.setState({
+            expression: value,
+            startNum: false,
+            })
+        }
+
+        else{
+          this.setState({
+            expression: this.state.expression + value,
+            })
+        }
+    }
+
+    return;
+  }
+
+
+  render() {
+    return (
+      <div className="calculator">
+      <OutputScreen value={this.state.expression}/>
+      <Keypad onClickHandler={this.calculatorOnClickHandler}/>
+      </div>
+    );
+  }
+}
+
+class OutputScreen extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {expression: '', result: this.props.value};
+  }
+
+  onClick = button => {
+    this.setState({
+      result: this.state.result + button
+    })
+  }
+
+  render() {
+    return (
+      <div className="output-screen">
+        {this.props.value}
+      </div>
+    );
+  }
+}
+
+class Keypad extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.buttonOnClick = this.buttonOnClick.bind(this);
+    this.calculatorHandler = this.props.onClickHandler;
+    
+  }
+
+  buttonOnClick = (event) => {
+    this.calculatorHandler(event.target.value);
+  }
+
+
+  render() {
+    return (
+      <div className="keypad">
+      <button className="keypad-num" onClick={this.buttonOnClick} value="7" >7</button>
+      <button className="keypad-num" onClick={this.buttonOnClick} value="8" >8</button>
+      <button className="keypad-num" onClick={this.buttonOnClick} value="9" >9</button>
+      <button className="keypad-button" onClick={this.buttonOnClick} value="*" >*</button>
+      <button className="keypad-num" onClick={this.buttonOnClick} value="4" >4</button>
+      <button className="keypad-num" onClick={this.buttonOnClick} value="5" >5</button>
+      <button className="keypad-num" onClick={this.buttonOnClick} value="6" >6</button>
+      <button className="keypad-button" onClick={this.buttonOnClick} value="%" >%</button>
+      <button className="keypad-num" onClick={this.buttonOnClick} value="1" >1</button>
+      <button className="keypad-num" onClick={this.buttonOnClick} value="2" >2</button>
+      <button className="keypad-num" onClick={this.buttonOnClick} value="3" >3</button>
+      <button className="keypad-button" onClick={this.buttonOnClick} value="-" >-</button>
+      <button className="keypad-num" onClick={this.buttonOnClick} value="0" >0</button>
+      <button className="keypad-button" onClick={this.buttonOnClick} value="AC" >AC</button>
+      <button className="keypad-button" onClick={this.buttonOnClick} value="=" >=</button>
+      <button className="keypad-button" onClick={this.buttonOnClick} value="+" >+</button>
+      </div>
+    );
+  }
+}
+
+
+
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Calculator />,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+// check expression for errors and return true if error is found
+function errorCheck(expression) {
+
+  // check if first char is not a - or a number
+  if (expression.charAt(0) !== '-' && isNaN(expression.charAt(0))){
+    return true;
+
+  }
+
+  // check if last char is not a number, or has length less than 3
+  if (expression.length < 3 || isNaN(expression.charAt((expression.length)-1))){
+    return true;
+  }
+
+  // check to see if the expression only has numbers
+  var hasOnlyNum = /^\d+$/.test(expression);
+  if (hasOnlyNum === true){
+    return true;
+  }
+
+  // check to see if there are more than 1 sign in a row
+  for (let i = 0; i < expression.length-1; i++){
+    if (isNaN(expression.charAt(i)) && isNaN(expression.charAt(i+1))){
+      return true;
+    }
+  }
+
+  return false; // no error was found in expression, return false
+
+}
+
+// parse and convert into an array to handle the expression 
+function parser(expression){
+
+  var res = expression.split("");
+  var new_expression = [];
+  var num = '';
+  var i = 0;
+  var first_negative = false;
+
+  // check if the first num should be negative
+  if (expression[0] === '-'){
+    first_negative = true;
+    i = 1;
+  }
+
+  for (i; i < res.length; i++){
+    if (isNaN(res[i])){
+
+      if (first_negative === true) {
+        new_expression.push(parseInt(num) * -1);
+        first_negative = false;
+      }
+      else{
+        new_expression.push(parseInt(num));
+      }
+
+      new_expression.push(res[i]);
+      num = '';
+    }
+    else{
+      num = num.concat(res[i]);
+    }
+
+  }
+  new_expression.push(parseInt(num));
+
+  return new_expression
+
+}
+
+
+// scan through expression following PEMDAS order 
+function calculateResult(expression) {
+  // multiplication
+  var scan1 = [];
+  var counter = 0;
+  while (counter < (expression.length)){
+
+    if (expression[counter] === "*"){
+      var first_num = scan1[scan1.length - 1]
+      var second_num = expression[counter+1];
+      var result = parseInt(first_num * second_num);
+      scan1.pop(); // remove last element
+      scan1.push(result);
+      counter += 1;
+
+    }
+    else{
+      scan1.push(expression[counter]);
+
+    }
+    counter += 1;
+  }
+
+  // division
+  var scan2 = [];
+  var counter = 0;
+  while (counter < (scan1.length)){
+
+    if (scan1[counter] === "%"){
+      var first_num = scan2[scan2.length - 1]
+      var second_num = scan1[counter+1];
+      if (second_num === 0){ // catch zero division error 
+        return "ERROR"; 
+        
+      }
+      var result = parseInt(first_num / second_num);
+      scan2.pop(); // remove last element
+      scan2.push(result);
+      counter += 1;
+
+    }
+    else{
+      scan2.push(scan1[counter]);
+
+    }
+    counter += 1;
+  }
+
+  // Addition and Subtraction
+  var scan3 = [];
+  var counter = 0;
+  while (counter < (scan2.length)){
+
+    if (scan2[counter] === "+"){
+      var first_num = scan3[scan3.length - 1]
+      var second_num = scan2[counter+1];
+      var result = parseInt(first_num + second_num);
+      scan3.pop(); // remove last element
+      scan3.push(result);
+      counter += 1;
+
+    }
+
+    else if (scan2[counter] === "-"){
+      var first_num = scan3[scan3.length - 1]
+      var second_num = scan2[counter+1];
+      var result = parseInt(first_num - second_num);
+      scan3.pop(); // remove last element
+      scan3.push(result);
+      counter += 1;
+
+    }
+
+    else{
+      scan3.push(scan2[counter]);
+
+    }
+    counter += 1;
+  }
+
+  return scan3;
+
+}
